50f3d5159875066b1f3e5ce51c6207fe9b8d382c
anasabbas575 <anasabbas503@gmail.com>
2026-03-04 07:20:51 +0500

    Update LAB_2 TASK_5.cpp

--- Lab 2/Lab Task/LAB_2 TASK_5.cpp
+++ Lab 2/Lab Task/LAB_2 TASK_5.cpp
@@ -1,8 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
-
+// Name: Anus Abbas Khan
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0723
+// Section: A
 
 
 
@@ -27,3 +26,4 @@ int main()
 
     return 0;  
 }
+
