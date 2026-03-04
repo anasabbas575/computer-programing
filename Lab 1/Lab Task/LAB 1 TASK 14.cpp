d980fc3608e6cc7d2c0caf251700719402b8a1b0
anasabbas575 <anasabbas503@gmail.com>
2026-03-04 07:14:22 +0500

    Update author details and maintain functionality

--- Lab 1/Lab Task/LAB 1 TASK 14.cpp
+++ Lab 1/Lab Task/LAB 1 TASK 14.cpp
@@ -1,8 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
-
+// Name: Anus Abbas Khan
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0723
+// Section: A
 
 #include <iostream>
 using namespace std;
@@ -26,3 +25,4 @@ int main()
     return 0;   // indicate that program ended successfully
     
     } // end function main
+
