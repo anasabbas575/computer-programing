8101cca09d411ee016416c6bf39bf602c765795c
anasabbas575 <anasabbas503@gmail.com>
2026-03-04 07:11:52 +0500

    Update LAB 1 TASK 11.cpp

--- Lab 1/Lab Task/LAB 1 TASK 11.cpp
+++ Lab 1/Lab Task/LAB 1 TASK 11.cpp
@@ -1,8 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
-
+// Name: Anus Abbas KHan
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0723
+// Section: A
 
     
     
@@ -26,4 +25,5 @@ int main()
     
     return 0;   // indicate that program ended successfully
     } // end function main
+
 
