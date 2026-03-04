2029fa85e8ed05562a3f404900d7425d73e3cf37
anasabbas575 <anasabbas503@gmail.com>
2026-03-04 07:23:58 +0500

    Update LAB 3 PRAC TASK 1.cpp

--- Lab 3/Lab Task/LAB 3 PRAC TASK 1.cpp
+++ Lab 3/Lab Task/LAB 3 PRAC TASK 1.cpp
@@ -1,7 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
+// Name: Anus Abbas Khan
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0723
+// Section: A
 
 
 #include <iostream>
@@ -18,4 +18,5 @@ int main(){
 		cout << "You are not elligible to vote." <<endl; //DISPLAY
 	}
 	return 0;
+
 }
