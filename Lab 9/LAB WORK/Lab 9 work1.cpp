543c8afdf5cc066cda6866fde76778c7fc48e5ce
anasabbas575 <anasabbas503@gmail.com>
2026-05-17 01:26:58 +0500

    Add files via upload

---
+++ Lab 9/LAB WORK/Lab 9 work1.cpp
@@ -0,0 +1,29 @@
+// Name: Anus Abbas Khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0723
+// Section: A
+
+
+ 
+//Function Introduction and Declaration
+//Declare a function called”greet”that takes no parameters and returns
+//  nothing.
+// Inside the function, display a greeting message of your choice.
+// Call the function from the main program to execute the greeting
+
+#include <iostream>
+using namespace std;
+
+// Function Declaration (Prototype)
+void greet();
+
+// Main Function
+int main() {
+    greet();   // function call
+    return 0;
+}
+
+// Function Definition
+void greet() {
+    cout << "Assalam o Alaikum! Welcome to C++ Functions.";
+}
