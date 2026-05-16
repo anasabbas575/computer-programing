86b9afea4bdd2254a7c1449981a652931ec481c6
anasabbas575 <anasabbas503@gmail.com>
2026-05-17 00:52:51 +0500

    Add files via upload

---
+++ Lab 6/Lab Work/lab 6 work 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a =17,s=34,d=18,f=19,g=19,h=20,j=34,k=45,l=45,z=45;
+	int average;
+	average =(a+s+d+f+g+h+j+k+l+z)/10;
+	cout<<"age of a is "<<a<<endl;
+	cout<<"age of s is "<<s<<endl;
+	cout<<"age of d is "<<d<<endl;
+	cout<<"age of f is "<<f<<endl;
+	cout<<"age of g is "<<g<<endl;
+	cout<<"age of h is "<<h<<endl;
+	cout<<"age of j is "<<j<<endl;
+	cout<<"age of k is "<<k<<endl;
+	cout<<"age of l is "<<l<<endl;
+	cout<<"age of z is "<<z<<endl;
+	cout<<"average  is "<<average<<endl;
+}
