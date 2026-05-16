1d3beedf315f7731434b96f780c990079cc37592
anasabbas575 <anasabbas503@gmail.com>
2026-05-17 01:28:07 +0500

    Add files via upload

---
+++ Lab11/LAB WORK/Lab 11 Lab Work 2.cpp
@@ -0,0 +1,25 @@
+// Name: Anus Abbas Khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0723
+// Section: A
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing first name
+    string firstName = "Shahzad ";
+
+    // Declaring and initializing last name
+    string lastName = "Faisal";
+
+    // Concatenating first name and last name with a space in between
+    string fullName = firstName + " " + lastName;
+
+    // Displaying the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
